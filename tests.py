2f0e698d098e91db19105c82df7a7e70b5d3e3b9
sahalsajjad <sahalsajjad@gmail.com>
2016-11-29 16:04:36 +0530

    Adding gp

--- tests.py
+++ tests.py
@@ -1,12 +1,9 @@
 import os
 
 def git_commit(directory):
-	print "Checking for %s" %(directory)
+	
 	lst = os.listdir(directory)
 	for x in lst:
-		if os.path.isdir(str(x)):
-			print "GIT COMMIT FOR %s " % (x)
-			git_commit(directory+"/"+x)
 		if os.path.isfile(str(x)):
 			os.system("git add %s" %(x))
 			os.system("git commit -m \"Adding gp\"")
