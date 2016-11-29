a6ba824fcd6465f7583b38b7a26e2fb88e3dc51b
sahalsajjad <sahalsajjad@gmail.com>
2016-11-29 16:00:08 +0530

    Adding gp

---
+++ tests.py
@@ -0,0 +1,16 @@
+import os
+
+def git_commit(directory):
+	print "Checking for %s" %(directory)
+	lst = os.listdir(directory)
+	for x in lst:
+		if os.path.isdir(str(x)):
+			print "GIT COMMIT FOR %s " % (x)
+			git_commit(directory+"/"+x)
+		elif os.path.isfile(str(x)):
+			os.system("git add %s" %(x))
+			os.system("git commit -m \"Adding gp\"")
+
+git_commit("gp")
+print "Added all files...\nPreparing for pushing to master\n"
+os.system("git push origin master")		
