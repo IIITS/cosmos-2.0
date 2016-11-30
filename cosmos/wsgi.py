bf6b269a7e1fdb411a49adeac38f871e55ca16b3
Sahal <sahalsajjad@gmail.com>
2016-11-30 14:38:28 +0530

    Adding gp

--- cosmos/wsgi.py
+++ cosmos/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cosmos.settings")
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cosmo.settings'
 application = get_wsgi_application()
