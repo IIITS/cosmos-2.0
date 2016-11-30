d73422af8852e2427ffa8e492a83e4838ce13086
sahalsajjad <sahalsajjad@gmail.com>
2016-11-30 15:18:19 +0530

    modified:   cosmos/urls.py 	modified:   cosmos/urls.pyc

--- cosmos/urls.py
+++ cosmos/urls.py
@@ -7,9 +7,9 @@ from btp import views as btp_views
 urlpatterns = [
 	url(r'^$', login_required(btp_views.IndexView.as_view()), name='homepage'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^btp/(?P<path>.*)$', include('btp.urls', namespace='btp')),
+    url(r'^btp/', include('btp.urls', namespace='btp')),
     url(r'^btp/static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    url(r'^issues-and-suggestions/(?P<path>.*)$', include('gp.urls')),
+    url(r'^issues-and-suggestions/', include('gp.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 handler404 = 'btp.views.page_not_found'
