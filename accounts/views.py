58f2f555c29371ef84efb5aa85db59fb879ce0c8
sahalsajjad <sahalsajjad@gmail.com>
2017-01-11 09:35:42 +0530

    Made changes

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from btp.models  import *
 from btp.forms   import *
 from btp.methods import *
+from accounts.forms import *
+from accounts.models import *
+from accounts.methods import *
 
 from django.core.exceptions import *
 from django.views.generic.base import TemplateView
@@ -20,7 +23,7 @@ from django.shortcuts import resolve_url,render
 from django.template.response import TemplateResponse
 from django.core.mail import send_mail
 import json
-
+from django.contrib.auth.models import User
 
 
 class LoginView(FormView):
@@ -47,6 +50,26 @@ class LoginView(FormView):
 			   'form':LoginForm(self.request.POST)	
 		}
 		return context
+
+class NewPasswordView(FormView):
+    template_name = 'accounts/login.html'
+    form_class = NewPasswordForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    def form_valid(self,form):
+        pass1 = form.cleaned_data['password']
+        pass2 = form.cleaned_data['password_again']
+        if pass1 == pass2:
+            print "password changed"
+            return HttpResponseRedirect('/')
+        return super(NewPasswordView,self).form_valid(form)
+    def form_invalid(self,form):
+        return render(self.request, self.template_name, {'form': form, 'form_error':'Sorry, username or password incorrect!' } )
+    def get_context_data(self,**kwargs):    
+        context = super(NewPasswordView,self).get_context_data(**kwargs)
+        context = {'title':'Login - Septem',
+               'form':NewPasswordForm(self.request.POST)  
+        }
+        return context        
 
 
 @sensitive_post_parameters()
@@ -86,3 +109,71 @@ class UnderConstruction(TemplateView):
 	template_name = 'index/underconstruction.html'
 	def dispatch(self, *args, **kwargs):
 		return super(UnderConstruction,self).dispatch(*args, **kwargs)
+
+class SendOtpView(FormView):
+
+    template_name = 'accounts/send_otp.html'
+    form_class=EmailPostForm
+    def get_context_data(self, **kwargs):
+        return {'form':EmailPostForm()}
+
+    def form_invalid(self,form):
+        return render(self.request, self.template_name, {'form': form, 'form_error':'Looks like you are not on cosmos!' } )
+    
+    def form_valid(self, form):
+            try:
+                user = User.objects.get(email=form.cleaned_data['email'])
+                name = user.get_full_name()
+                subject = "[Cosmos@IIITS] Password Reset - One Time Password"
+                to = [user.email]
+                bcc = ["sahalsajjad@gmail.com"]
+                from_email = '[do-not-reply] Cosmos support <no-reply@cosmos.iiits.in>'
+                ctx = {
+                    'title':'[Cosmos] Password Reset - OTP',
+                    'otp':createOTP(),
+                    "name":name
+                }
+
+                message = get_template('email/otp_email.html').render(Context(ctx))
+                msg = EmailMessage(subject, message, to=to, from_email=from_email, bcc=bcc)
+                msg.content_subtype = 'html'
+                msg.send()
+                return HttpResponseRedirect('/accounts/verify-otp/lost-password/?user_email=%s' %(user.email))   
+
+            except ObjectDoesNotExist:   
+                return render(self.request, self.template_name, {'form': form, 'form_error':'The email you entered is not associated with any account on cosmos!'})
+
+class VerifyOtpView(FormView):
+
+    template_name = 'accounts/verify_otp.html'   
+    form_class = OTPForm
+    success_url = '/accounts/new-password/'
+    
+    def form_valid(self, form):
+            try:
+                otp = form.cleaned_data['otp']
+                
+                subject = "[Cosmos@IIITS] Password Reset - One Time Password"
+                to = [user_email]
+                bcc = ["sahalsajjad@gmail.com"]
+                from_email = '[do-not-reply] Cosmos support <no-reply@cosmos.iiits.in>'
+                ctx = {
+                    'title':'[Cosmos] Password Reset - OTP',
+                    'otp':otp,
+                    "name":name
+                }
+
+                message = get_template('email/otp_email.html').render(Context(ctx))
+                msg = EmailMessage(subject, message, to=to, from_email=from_email, bcc=bcc)
+                msg.content_subtype = 'html'
+                msg.send()
+                return HttpResponseRedirect(settings.LOGIN_URL)    
+
+            except ObjectDoesNotExist:   
+                return render(self.request, self.template_name, {'form': form, 'form_error':'The email you entered is not associated with any account on cosmos!'})
+            
+    def form_invalid(self,form):
+        return render(self.request, self.template_name, {'form': form, 'form_error':'OTP entered is incorrect. Please check your email' } )
+    
+    def get_context_data(self,**kwargs):
+        return {'email':self.kwargs['user_email']}           
