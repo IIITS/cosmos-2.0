fb30d9da14d62082a5de3a20bff5916ec3ce9a4a
Sahal <sahalsajjad@gmail.com>
2017-02-28 15:17:14 +0530

    Add ldap authentication :lock:

--- cosmos/settings.py
+++ cosmos/settings.py
@@ -2,6 +2,7 @@ import os
 import ldap
 from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 import django
+from pswd import LDAP_PASS, LDAP_USER
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,12 +24,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     #'rest_framework',
-    'ldap_sync',
-    'btp',
-    'accounts',
-    'gp',
-    'feasta',
-    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +36,12 @@ INSTALLED_APPS = [
     'pagedown',
     'comments',
     'posts',
+    'ldap_sync',
+    'btp',
+    'accounts',
+    'gp',
+    'feasta',
+    'polls',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -107,23 +108,24 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_LDAP_SERVER_URI = "ldap.iiits.in"
 
+AUTH_LDAP_BIND_DN = LDAP_USER
+AUTH_LDAP_BIND_PASSWORD = LDAP_PASS
+AUTH_LDAP_CONNECTION_OPTIONS = {
+    ldap.OPT_DEBUG_LEVEL: 1,
+    ldap.OPT_REFERRALS: 0,
+}
 
-AUTH_LDAP_BIND_DN = "cn=django-agent,dc=iiits,dc=in"
-AUTH_LDAP_BIND_PASSWORD = "iiits@123"
 AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=people,dc=iiits,dc=in",
     ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
-# or perhaps:
-# AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,ou=people,dc=iiits,dc=in"
-
-# Set up the basic group parameters.
 AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=django,ou=groups,dc=iiits,dc=in",
     ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)"
 )
 AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
 
-# Simple group restrictions
-AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=iiits,dc=in"
-AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=iiits,dc=in"
+
+# Cache settings
+AUTH_LDAP_CACHE_GROUPS = True
+AUTH_LDAP_GROUP_CACHE_TIMEOUT = 300
 
 # Populate the Django user from the LDAP directory.
 AUTH_LDAP_USER_ATTR_MAP = {
@@ -132,34 +134,8 @@ AUTH_LDAP_USER_ATTR_MAP = {
     "email": "mail"
 }
 
-AUTH_LDAP_PROFILE_ATTR_MAP = {
-    "employee_number": "employeeNumber"
-}
-
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    "is_active": "cn=active,ou=django,ou=groups,dc=iiits,dc=in",
-    "is_staff": "cn=staff,ou=django,ou=groups,dc=iiits,dc=in",
-    "is_superuser": "cn=superuser,ou=django,ou=groups,dc=iiits,dc=in"
-}
-
-AUTH_LDAP_PROFILE_FLAGS_BY_GROUP = {
-    "is_awesome": "cn=awesome,ou=django,ou=groups,dc=iiits,dc=in",
-}
-
-# This is the default, but I like to be explicit.
-AUTH_LDAP_ALWAYS_UPDATE_USER = True
-
-# Use LDAP group membership to calculate group permissions.
 AUTH_LDAP_FIND_GROUP_PERMS = True
 
-# Cache group memberships for an hour to minimize LDAP traffic
-AUTH_LDAP_CACHE_GROUPS = True
-AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
-
-#LDAP SETTINGS
-
-LDAP_SYNC_URI = "ldap://ldap.iiits.in"
-LDAP_SYNC_BASE = "OU=people,DC=iiits,DC=in"
 # Keep ModelBackend around for per-user permissions and maybe a local
 # superuser.
 AUTHENTICATION_BACKENDS = (
